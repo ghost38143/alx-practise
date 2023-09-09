f5da99ec7a72dfd9a2a811e2be507050c62a073a
ghost38143 <ghost38143@gmail.com>
2023-09-09 15:33:04 -0700

    begin of c language

---
+++ hello.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+
+int main(void)
+{
+	printf("hello, World!\n");
+	printf("My name is :%s i am %d years old ,i weigh %2.f, i am an %c student \n", "silvvya", 22,64.67,'A');
+		return (0);
+}
