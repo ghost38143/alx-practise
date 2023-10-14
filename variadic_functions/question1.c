9e8ccde1e0d3e1133c9f92b1ca05c761b7787e5a
ghost38143 <ghost38143@gmail.com>
2023-10-14 05:44:13 -0700

    printing the string sung the variadic function

--- variadic_functions/question1.c
+++ variadic_functions/question1.c
@@ -1,15 +1,19 @@
 #include <stdio.h>
 #include <stdarg.h>
+#include <string.h>
 
 int max(int num_args, ...);
+void print(char *placeholder, ...);
 /**
  * main - call the maximum number
  * Return: o always success
  */
 int main(void)
 {
-	int max_num = max(5, 26, 17, 4, 0, 13);
+	int max_num = max(9, 26, 17, 4, 0, 13, 78, 67, 90, 10);
 	printf("max_num: %d\n", max_num);
+	print("ddfd", 5, 6, 7.8, 1);
+	print("ffdd", 7.2, 8.5, 7, 3);
 	return (0);
 }
 /**
@@ -39,4 +43,37 @@ int max(int num_args, ...)
 	}
 	va_end(args);
 	return (max);
+}
+
+/**
+ * print - print a string of charcters
+ * @placeholders: the string of acharcters
+ * @...: the rst of the arguments
+ * Return: 0
+ */
+void print(char *placeholder, ...)
+{
+	int length = strlen(placeholder);
+
+	va_list args;
+	int i;
+
+	va_start(args, placeholder);
+
+	for (i = 0; i < length; i++)
+	{
+		if (placeholder[i] == 'd')
+		{
+			int x = va_arg(args, int);
+			printf("%d", x);
+			printf("\n");
+		}
+		else if (placeholder[i] == 'f')
+		{
+			double x = va_arg(args, double);
+			printf("%f", x);
+			printf("\n");
+		}
+	}
+	va_end(args);
 }
